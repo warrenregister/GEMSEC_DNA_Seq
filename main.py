4e78dc5dfb6f4849ccd032676110d53a123db784
warrenregister <42283094+warrenregister@users.noreply.github.com>
2019-10-07 17:21:36 -0700

    Updated main to allow for direct file merging

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import user_interface
+from user_interface import user_interface
 
-gui = user_interface.user_interface()
+# Prompt user for whether they want to convert 1 FASTQ file, convert a folder of FASTQ files,
+# or merge a folder of CSVs (previously converted from FASTQ) into 1 CSV
+def main():
+    gui = user_interface()
 
-if gui.reply == 0:
-    gui.convert_single_file()
-else:
-    gui.convert_directory()
+    if gui.reply == 0:  # User chose to convert 1 file
+        gui.convert_single_file()
+    elif gui.reply == 1:  # User chose to convert a folder of files
+        gui.convert_directory()
+    else:  # User chose to merge a set of files
+        gui.merge_directory()
+
+
+main()
