9b615a7a2499e62548a67b51c66825c72722dff6
warrenregister <42283094+warrenregister@users.noreply.github.com>
2020-01-14 17:42:23 -0800

    changed basic path info

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sequence_csv_merger import sequence_csv_merger
 # Prompt user for whether they want to convert 1 FASTQ file, convert a folder of FASTQ files,
 # or merge a folder of CSVs (previously converted from FASTQ) into 1 CSV
 def main():
-    directory = 'Desktop/GEMSEC_DNA_Seq/test_fqs'
+    directory = '/Users/warren/Desktop/docs/GEMSEC_DNA_Seq/FASTQ_files/realFASTQ'
     for fileName in os.listdir(directory):
             if fileName.split('.')[-1] in ['fq', 'FASTQ', 'fastq']:
                 extractor = sequence_extractor(directory + '/' + fileName)
