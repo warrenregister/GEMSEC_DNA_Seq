3ed2fb1b76aefefd5bd6ef3802a61e925a67dcf7
JacksonFrank <jacksonrfrank@gmail.com>
2020-01-16 13:46:05 -0800

    Add command line arg for FASTQ directory choosing

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import sys
 from sequence_extractor import sequence_extractor
 from sequence_csv_merger import sequence_csv_merger
 
 # Prompt user for whether they want to convert 1 FASTQ file, convert a folder of FASTQ files,
 # or merge a folder of CSVs (previously converted from FASTQ) into 1 CSV
 def main():
-    directory = '/Users/warren/Desktop/docs/GEMSEC_DNA_Seq/FASTQ_files/realFASTQ'
+    directory = sys.argv[1] # uses the given directory (1st command line arg) to look at the FASTQ files
     for fileName in os.listdir(directory):
             if fileName.split('.')[-1] in ['fq', 'FASTQ', 'fastq']:
                 extractor = sequence_extractor(directory + '/' + fileName)
