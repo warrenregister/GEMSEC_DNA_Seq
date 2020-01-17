3b4887e21a692f0dbb9c797003a5371686ee075f
JacksonFrank <jacksonrfrank@gmail.com>
2020-01-16 22:46:26 -0800

    Add process indication messages

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from sequence_csv_merger import sequence_csv_merger
 # or merge a folder of CSVs (previously converted from FASTQ) into 1 CSV
 def main():
     directory = sys.argv[1] # uses the given directory (1st command line arg) to look at the FASTQ files
-    print("Converting files from " + directory + " directory.")
-    directory = sys.argv[1] # uses the given directory (1st command line arg) to look at the FASTQ files
+    print("Extracting files from " + directory + " directory...")
     for fileName in os.listdir(directory):
             if fileName.split('.')[-1] in ['fq', 'FASTQ', 'fastq']:
                 extractor = sequence_extractor(directory + '/' + fileName)
                 extractor.extract()
+                print("Files extracted, converting files to CSV format...")
                 extractor.write_CSV()
     
     merger = sequence_csv_merger(directory + '/')
